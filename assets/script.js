d5beb1e4218acdf12dc0d39b28ed2ee9985504d8
SSPENELOPE <typoepping@gmail.com>
2022-07-19 11:44:49 -0400

    working, need to fix extra character on lenght

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,49 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var characters = {
+  lowercase: "abcdefghijklmnopqrstuvwxy",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  number: "0123456789",
+  symbols: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+};
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var passwordCharacters = "";
+  var passwordLength = prompt("Choose a password lenght between 8 and 128");
+  if (passwordLength) {
+  } else {
+    return;
+  }
+
+  var lowercase = confirm("Would you like to use lowercase letters?");
+  if (lowercase) {
+    passwordCharacters += characters.lowercase;
+  };
+
+  var uppercase = confirm("would you like to use uppercase letters?");
+  if (uppercase) {
+    passwordCharacters += characters.uppercase;
+  };
+
+  var number = confirm("Would you like to use numbers?");
+  if (number) {
+    passwordCharacters += characters.number;
+  };
+
+  var symbols = confirm("Would you like to add symbols?");
+  if (symbols) {
+    passwordCharacters += characters.symbols;
+  };
+
+
+  var password = "";
+  for (var i = 0; i <= passwordLength; i++) {
+    password += passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+  }
+
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -12,3 +52,12 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+/*var password = generatePassword(); {
+  password = "";
+  for (var i = 0; i <= passwordLength; i++) {
+    password = passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+  }
+};*/
